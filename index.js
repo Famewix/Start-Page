10cc84d9a65c59d1e670abfdd4602eec177ff2ff
Faridin Islam <faridin05saif@gmail.com>
2021-12-27 22:09:33 +0600

    added background image.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const hourE = document.querySelector('.hour');
-const minuteE = document.querySelector('.minute');
-const secondE = document.querySelector('.second');
+const hourE = document.querySelector(".hour");
+const minuteE = document.querySelector(".minute");
+const secondE = document.querySelector(".second");
 
 function changeTime() {
-    var d = new Date();
-    hour = d.getHours();
-    minute = d.getMinutes();
-    second = d.getSeconds();
-    
-    hourE.innerHTML = hour
-    minuteE.innerHTML = minute
-    secondE.innerHTML = second
-    // console.log(hour, minute, second)
+  var d = new Date();
+  hour = d.getHours();
+  minute = d.getMinutes();
+  second = d.getSeconds();
+
+  hourE.innerHTML = hour;
+  minuteE.innerHTML = minute;
+  secondE.innerHTML = second;
+  // console.log(hour, minute, second)
 }
 
-changeTime()
+changeTime();
 
-setInterval(changeTime, 1000)
+setInterval(changeTime, 1000);
